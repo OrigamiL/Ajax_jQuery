298d7a6b2fae3a21097d98b926a0744c3dd2c933
OrigamiL <1714573172@qq.com>
2022-05-23 17:23:39 +0800

    Initial commit

---
+++ ajax/src/com/weifang/javaweb/servlet/AjaxRequest1Servlet.java
@@ -0,0 +1,21 @@
+package com.weifang.javaweb.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/ajaxRequest1")
+public class AjaxRequest1Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.print("<font color='red'>ajax！</font>");
+    }
+}
---
+++ old/web/index.jsp
@@ -0,0 +1,24 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>old请求</title>
+  </head>
+
+  <body>
+  <script type="text/javascript">
+    /*该种声明方式需要在调用之前
+    function sendRequest(){}则调用可以写在声明之前*/
+    sendRequest = function (){
+      document.location.href = "/old/request"
+    }
+
+  </script>
+  <a href="/old/request">超链接</a>
+  <br>
+  <form action="/old/request">
+    <input type="submit" value="提交"/>
+  </form>
+  <input type="button" onclick="sendRequest()" value="JS代码发送请求" />
+
+  </body>
+</html>
